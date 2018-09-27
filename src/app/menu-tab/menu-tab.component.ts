4baab11cb094ba4bd1c0d39b75df8b3560089f30
spinillag@viewnext.com <spinillag@viewnext.com>
2018-09-27 20:33:26 +0200

    Initial Commit

---
+++ src/app/menu-tab/menu-tab.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {TabViewModule} from 'primeng/tabview';
+
+@Component({
+  selector: 'vn-menu-tab',
+  templateUrl: './menu-tab.component.html',
+  styleUrls: ['./menu-tab.component.css']
+})
+export class MenuTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
